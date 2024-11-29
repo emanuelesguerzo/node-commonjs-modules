d195d1eb911b15d28dfc680da4908130654b8bf1
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2024-11-29 13:23:14 +0100

    feat: names.js complete

---
+++ modules/names.js
@@ -0,0 +1,14 @@
+
+function fullNameObj(firstName, lastName) {
+    let personFullName = {
+        firstName,
+        lastName,
+    };
+    return personFullName;
+}
+
+module.exports = {
+    fullName: fullNameObj,
+}
+
+
